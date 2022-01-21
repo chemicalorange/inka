c3d340b0b0f9f989ce1e99bfa9a67dc39518f714
chemicalorange <66693973+chemicalorange@users.noreply.github.com>
2022-01-21 19:33:13 +0700

    1.2s

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -1,7 +1,22 @@
 
 gsap.registerPlugin(ScrollTrigger)
 
-gsap.to('.one-app__brands-item, .one-app__reviews-item', {
+gsap.to('.one-app__brands-item', {
+    scrollTrigger: {
+        trigger: '.one-app__brands-item',
+        start: 'top center',
+        end: 'bottom',
+        toggleActions: 'restart pause reverse reverse',
+        scrub: true
+    },
+    x:0,
+    y:0,
+    background: '#7171FF',
+    rotation: 0, 
+    duration: 0.5
+})
+
+gsap.to('.one-app__reviews-item', {
     scrollTrigger: {
         trigger: '.one-app__brands-item',
         start: '50px center',
@@ -9,11 +24,58 @@ gsap.to('.one-app__brands-item, .one-app__reviews-item', {
         toggleActions: 'restart pause reverse reverse',
         scrub: true
     },
-    x:0,
-    y:0,
+    opacity:0.3,
     rotation: 0, 
-    duration: 0.5
+    duration: 0.5,
+    y:0,
+    x:0,
 })
+
+gsap.to('.one-app__logo, .one-app__logo img', {
+    scrollTrigger: {
+        trigger: '.one-app__brands-item',
+        start: '10px center',
+        end: 'bottom',
+        toggleActions: 'restart pause reverse reverse',
+        scrub: true
+    },
+    background: '#7171FF',
+    rotation: 0, 
+    duration: 2,
+    delay: 0.8,
+    opacity:1
+})
+
+gsap.to('.one-app__reviews-item', {
+    scrollTrigger: {
+        trigger: '.one-app__brands-item',
+        start: '50px center',
+        end: 'bottom',
+        toggleActions: 'restart pause reverse reverse',
+        scrub: true
+    },
+    
+    opacity:1,
+    rotation: 0, 
+    duration: 0.5,
+    delay: 0.8,
+    
+})
+
+gsap.to('.one-app__description', {
+    scrollTrigger: {
+        trigger: '.one-app__reviews',
+        start: '50px center',
+        end: 'bottom',
+        toggleActions: 'restart pause reverse reverse',
+        scrub: true
+    },
+    
+    opacity:1,
+    duration: 0.5,
+    
+})
+
 
 let banners = document.querySelectorAll('.banner');
 
