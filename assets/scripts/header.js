fe3401762f87f678b8aa729bea229871f51f1c39
chemicalorange <66693973+chemicalorange@users.noreply.github.com>
2022-01-26 17:12:23 +0700

    update 01.29.2022

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -8,3 +8,24 @@ window.addEventListener('scroll', ()=>{
     }
     
 })
+
+const burgerMenu = document.querySelector('.header__menu')
+const nav = document.querySelector('.header__nav')
+const navItems = document.querySelectorAll('.nav__has-child')
+
+burgerMenu.addEventListener('click', ()=>{
+    burgerMenu.classList.toggle('active')
+    header.classList.add('white')
+    nav.classList.toggle('nav_active')
+    navItems[0].classList.add('active')
+    document.querySelector('body').classList.toggle('lock')
+})
+
+
+for(let item of navItems){
+    item.addEventListener('click', ()=>{
+        navItems.forEach( item2 => item2.classList.remove('active'))
+        item.classList.add('active')
+        
+    })
+}
