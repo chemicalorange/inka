1344181a90c0ecbf20dca42ff8882ba3f5c10eb6
chemicalorange <66693973+chemicalorange@users.noreply.github.com>
2022-02-21 21:53:08 +0700

    02.21.2022

--- assets/scripts/animation-text.js
+++ assets/scripts/animation-text.js
@@ -4,8 +4,20 @@ let banners = document.querySelectorAll('.banner');
 
 for(let banner of banners){
     let bannerContent = banner.querySelector('.banner__content')
+    let bannerImage = banner.querySelector('.banner__image')
 
-    gsap.to(bannerContent, {
+    gsap.to(bannerContent,{
+        scrollTrigger:{
+            trigger: banner,
+            start: '20px center',
+            end: "400px",
+        },
+        opacity:1,
+        y:0,
+        duration: 0.5
+    })
+
+    gsap.to(bannerImage,{
         scrollTrigger:{
             trigger: banner,
             start: '20px center',
