1da03760db60bbd07ff444a3f83bfd2f19a46ee1
chemicalorange <66693973+chemicalorange@users.noreply.github.com>
2022-04-10 16:48:45 +0700

    10.04.2022

--- assets/scripts/header-color.js
+++ assets/scripts/header-color.js
@@ -1,8 +1,11 @@
+let headerJoinBtnImg = document.querySelector('.header__button img')
 window.addEventListener('scroll', ()=>{
     if(window.pageYOffset > document.querySelector('.first-screen').clientHeight){
         header.classList.add('white')
+        headerJoinBtnImg.src = 'assets/images/icon-join-white.svg' 
     }else{
         header.classList.remove('white')
+        headerJoinBtnImg.src = 'assets/images/icon-join.svg' 
     }
     
 })
